3e5c133a497e2dad80390747e57b6f8157287268
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-05-22 16:27:58 +0530

    Add Header component and implement user authentication pages for login and registration

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import Providers from "./components/Providers";
+import Header from "./components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          {/* <Header /> */}
+          <Header />
           <main className="container mx-auto px-4 py-8">{children}</main>
         </Providers>
       </body>
